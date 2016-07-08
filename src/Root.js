c0978ed1333ba58d5848485d80e3f14de33f3ecc
Srijak <srijak@gmail.com>
2016-07-08 11:04:55 -0400

    combine

---
+++ src/Root.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export default class starter extends Component {
+  render() {
+    const txt = Platform.OS === 'ios' ? 
+		<Text style={styles.instructions}>
+		  Press Cmd+R to reload,{'\n'}
+		  Cmd+D or shake for dev menu
+		</Text> :
+		<Text style={styles.instructions}>
+		  Cmd+M or shake for dev menu
+		</Text>;
+
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+	{txt}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
